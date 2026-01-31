b9321fd9bcd26c4bb459ab1ea7ab6987b29e7f9e
Nguyen Huy <nguyenhuyltta158@gmail.com>
2026-01-31 11:01:51 +0700

    Upload project lab3

---
+++ 2324802010238_NguyenQuangHuy_E.API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _2324802010238_NguyenQuangHuy_E.API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
